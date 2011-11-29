d86041afa759cad90e2a2855b4a399434226db14
Phil Monroe <phil@philmonroe.com>
2011-11-28 18:06:15 -0800

    adding control panel page

--- lib/powser/server.rb
+++ lib/powser/server.rb
@@ -1,52 +1,57 @@
 require 'sinatra'
+require 'powder'
 
 module Powser  
   class Server < Sinatra::Base
     POW_PATH = `powder config | grep hostRoot | sed s/hostRoot//`.strip
   
     before do
-      @pow_app = `cat app`.strip
+      @default_app = `cat app`.strip
     end
     
     # Routes ------------------------------------------------------------------
+    get '/' do
+      @apps = pow_apps
+      erb :index
+    end
+    
     get '/restart' do
-      @pow_app = params[:app] if params[:app]
+      app = params[:app] || @default_app
       
       to_url = redirect_url(params[:redirect])
-      pow_dir(@pow_app) do
+      pow_dir(app) do
         `powder restart`
-        to_url ||= pow_url(@pow_app)
+        to_url ||= pow_url(app)
       end
       redirect to_url
     end
     
     private
     
-    # Helpers -------------------------------------------------------------------
+    # Class Methods -------------------------------------------------------------------
     def pow_dir(app_name, &block)
       path = POW_PATH + '/' + app_name
       Dir.chdir(path) do
         yield
       end
     end
-
-    def pow_url(app_name)
-      'http://' + app_name + '.' + domain
-    end
     
     def redirect_url(to)
       "http://" + to if to
     end
-
-    def domain
-      if File.exists? File.expand_path('~/.powconfig')
-        returned_domain = %x{source ~/.powconfig; echo $POW_DOMAINS}.gsub("\n", "").split(",").first
-        returned_domain = %x{source ~/.powconfig; echo $POW_DOMAIN}.gsub("\n", "") if returned_domain.nil? || returned_domain.empty?
-        returned_domain = 'dev' if returned_domain.nil? || returned_domain.empty?
-        returned_domain
-      else
-        'dev'
+    
+    include Powder::Helpers
+    
+    # Helpers
+    helpers do
+      def pow_url(app_name)
+        'http://' + app_name + '.' + domain
       end
+      
+      def restart_link(app_name)
+        "<a href='/restart?app=#{app_name}'>restart</a>"
+      end
+      
     end
   end
 end
